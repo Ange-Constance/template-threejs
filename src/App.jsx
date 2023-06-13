6183f2a746719af8e785c944fab271f197b823fc
“Ange-Constance” <constance.nimuhire@gmail.com>
2023-06-13 19:18:43 +0200

    set up

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="w-full text-3xl h-screen font-bold text-blue-700 items-center flex justify-center underline">
+      Hello world, tailwind css!
+    </h1>
+  )
+}
